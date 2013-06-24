151ccfcadea5d784fb1f3318c9cd9d29eaca59fb
Johnny Shields <johnny.shields@gmail.com>
2013-06-25 04:48:41 +0900

    - Eliminate duplication in spec_helper.rb - Use Ruby 1.8 hash syntax

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,12 +18,19 @@ ActiveRecord::Schema.define do
   end
 end
 
+module SharedModelMethods
+  extend ActiveSupport::Concern
+  included do
+    attr_accessor :phone_method, :phone1_method, :country_code
+    phony_normalized_method :phone_attribute # adds normalized_phone_attribute method
+    phony_normalized_method :phone_method # adds normalized_phone_method method
+    phony_normalized_method :phone1_method, :default_country_code => 'DE' # adds normalized_phone_method method
+    phony_normalize :phone_number # normalized on validation
+  end
+end
+
 class ActiveRecordModel < ActiveRecord::Base
-  attr_accessor :phone_method, :phone1_method, :country_code
-  phony_normalized_method :phone_attribute # adds normalized_phone_attribute method
-  phony_normalized_method :phone_method # adds normalized_phone_method method
-  phony_normalized_method :phone1_method, :default_country_code => 'DE' # adds normalized_phone_method method
-  phony_normalize :phone_number # normalized on validation
+  include SharedModelMethods
 end
 
 class ActiveRecordDummy < ActiveRecordModel
@@ -32,14 +39,10 @@ end
 class MongoidModel
   include Mongoid::Document
   include Mongoid::Phony
-  field :phone_attribute, type: String
-  field :phone_number,    type: String
-  field :phone_number_as_normalized, type: String
-  attr_accessor :phone_method, :phone1_method, :country_code
-  phony_normalized_method :phone_attribute # adds normalized_phone_attribute method
-  phony_normalized_method :phone_method # adds normalized_phone_method method
-  phony_normalized_method :phone1_method, :default_country_code => 'DE' # adds normalized_phone_method method
-  phony_normalize :phone_number # normalized on validation
+  field :phone_attribute, :type => String
+  field :phone_number,    :type => String
+  field :phone_number_as_normalized, :type => String
+  include SharedModelMethods
 end
 
 class MongoidDummy < MongoidModel
